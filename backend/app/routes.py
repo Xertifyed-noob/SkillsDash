e91e4bdb88ed49692fa0de06551a56acc445250f
Xertifyed-noob <Richard.wang040@gmail.com>
2024-06-19 20:45:35 +0800

    feat: Implement backend filtering for job titles
    - Changed the database schema to have a column for job title
    - Edited view handlers to query data using job title filter

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .models import Skill, Tool, Education
 
 main = Blueprint('main', __name__)
@@ -6,15 +6,28 @@ main = Blueprint('main', __name__)
 # Define view handlers to query records from the respective databases and return in JSON format
 @main.route('/skills', methods=['GET'])
 def get_skills():
-    skills = Skill.query.all()
+    job_title = request.args.get('job_title')
+    if job_title:
+        skills = Skill.query.filter_by(job_title=job_title).all()
+    else:
+        skills = Skill.query.all()
     return jsonify([skill.to_dict() for skill in skills])
 
 @main.route('/tools', methods=['GET'])
 def get_tools():
-    tools = Tool.query.all()
+    job_title = request.args.get('job_title')
+    if job_title:
+        tools = Tool.query.filter_by(job_title=job_title).all()
+    else:
+        tools = Tool.query.all()
     return jsonify([tool.to_dict() for tool in tools])
+
 
 @main.route('/education', methods=['GET'])
 def get_education():
-    education = Education.query.all()
+    job_title = request.args.get('job_title')
+    if job_title:
+        education = Education.query.filter_by(job_title=job_title).all()
+    else:
+        education = Education.query.all()
     return jsonify([edu.to_dict() for edu in education])
