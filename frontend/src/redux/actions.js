fa878ee2f720011f797d48f2b0a23733c61408eb
Xertifyed-noob <Richard.wang040@gmail.com>
2024-07-02 14:44:57 +0800

    fix: resolve 404 error in API request
    - Updated the API endpoint URL to ensure the correct path is used
    - Verified that the backend flask app is running at specified endpoint

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -1,9 +1,34 @@
 import axios from 'axios';
 
+const BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/aggregated`;
+
 // Action creator for fetching data based on job title
 export const fetchData = (jobTitle) => async (dispatch) => {
-    // Make GET API request to fetch data for a given job title
-    const response = await axios.get(`/api/data?jobTitle=${jobTitle}`);
-    // Dispatch action with fetched data
-    dispatch({ type: 'FETCH_DATA', payload: response.data });
+    // Create GET API requests to fetch data for a given job title
+    try {
+        const skillsResponse = await axios.get(`${BASE_URL}/skills`, {
+            params: { job_title: jobTitle }
+        });
+        const toolsResponse = await axios.get(`${BASE_URL}/tools`, {
+            params: { job_title: jobTitle }
+        });
+        const educationResponse = await axios.get(`${BASE_URL}/education`, {
+            params: { job_title: jobTitle }
+        });
+        const summaryResponse = await axios.get(`${BASE_URL}/summary`, {
+            params: { job_title: jobTitle }
+        });   
+
+        const data = {
+            skills: skillsResponse.data,
+            tools: toolsResponse.data,
+            education: educationResponse.data,
+            summaryStats: summaryResponse.data
+        };
+
+        // Dispatch action with fetched data
+        dispatch({ type: 'FETCH_DATA', payload: data });
+    }  catch (error) {
+        console.error('Error fetching data:', error);
+    }
 };
