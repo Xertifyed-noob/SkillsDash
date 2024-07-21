6e3694a323b413259f57fc51385e33dc8323d561
Xertifyed-noob <Richard.wang040@gmail.com>
2024-07-21 22:08:38 +0800

    refactor: Update backend job title filtering
    - Made aggregate_data() function handle the job title filtering logic in data_analysis.py
    - Consolidated all the API endpoints into a single endpoint in routes.py for efficient processing

--- backend/app/data_analysis.py
+++ backend/app/data_analysis.py
@@ -1,24 +1,21 @@
 import pandas as pd
-from app import db, create_app
+from app import db
 from app.models import Skill, Tool, EducationLevel, FieldOfStudy
 
 # Function to aggregate a given column (skill, tool, education_level) of a MySQL table
-def aggregate_query(model, column, job_title=None):
+def aggregate_query(model, column):
     # Count the occurence of each unique item under the column
     query = db.session.query(column, db.func.count(column).label('count'))
-    # Job title filtering
-    if job_title:
-        query = query.filter(model.job_title == job_title)
     # Combine rows with the same item under the column
     query = query.group_by(column).all()
     return pd.DataFrame(query, columns=[column.name, 'count'])
 
 
 # Function to further aggregate tool column to include the counts of their appearances alongside any of the top skills [Originally used for BubbleChart.js]
-def aggregate_tools(job_title=None):
-    top_skills = get_top_skills(job_title)
+def aggregate_tools():
+    top_skills = get_top_skills()
     # Aggregate the tool column and return a data frame of tool counts
-    tool_counts = aggregate_query(Tool, Tool.tool, job_title)
+    tool_counts = aggregate_query(Tool, Tool.tool)
     tool_counts['co_appearance'] = 0
 
     for tool in tool_counts['tool']:
@@ -27,9 +24,6 @@ def aggregate_tools(job_title=None):
         for skill in top_skills:
             # Query to count the number of times the tool appears alongside the current skill (number of unique job IDs)
             query = db.session.query(Tool.job_id).join(Skill, Skill.job_id == Tool.job_id).filter(Tool.tool == tool, Skill.skill == skill)
-            # Job title filtering
-            if job_title:
-                query = query.filter(Tool.job_title == job_title)
             result = query.all()   
             # Ensures that each job ID is counted only once
             for row in result:
@@ -41,15 +35,12 @@ def aggregate_tools(job_title=None):
 
 
 # Function to query the top 3 skills from the Skill table in MySQL database [Originally used for BubbleChart.js]
-def get_top_skills(job_title=None, n=3):
+def get_top_skills(n=3):
     # Base query to count the occurence of each unique skill
     query = db.session.query(
         Skill.skill,
         db.func.count(Skill.skill).label('count')
     ).group_by(Skill.skill)
-    # Job title filtering
-    if job_title:
-        query = query.filter(Skill.job_title == job_title)
     # Sort in descending order and limit the query result to the top n skills with the most counts
     top_skills_query = query.order_by(db.desc('count')).limit(n).all()
     # Extract the skill names from the query result
@@ -58,7 +49,7 @@ def get_top_skills(job_title=None, n=3):
 
 
 # Function to aggregate the field of study column separately
-def aggregate_field_of_study(job_title=None):
+def aggregate_field_of_study():
     # Count the occurence of each unique field of study
     query = db.session.query(
         EducationLevel.education_level,
@@ -66,28 +57,14 @@ def aggregate_field_of_study(job_title=None):
         db.func.count(FieldOfStudy.field_of_study).label('count')
     # Joins FieldOfStudy table with EducationLevel table based on the condition that id of latter matches education_level_id of former
     ).join(FieldOfStudy, EducationLevel.id == FieldOfStudy.education_level_id) 
-    # Job title filtering
-    if job_title:
-        query = query.filter(FieldOfStudy.job_title == job_title)
     # Groups the counts first by education level, and then field of study
     query = query.group_by(EducationLevel.education_level, FieldOfStudy.field_of_study).all()
     return pd.DataFrame(query, columns=['education_level', 'field_of_study', 'count'])
 
 
-# Function to count the total number of unique items under a given column
-def count_distinct(model, column, job_title=None):
-    query = db.session.query(column).distinct()
-    if job_title:
-        query = query.filter(model.job_title == job_title)
-    return query.count()
-
-def calculate_summary_stats(data, job_title=None):
+def calculate_summary_stats(data):
     # Create a copy of the original data
     data = data.copy()
-
-    # Job title filtering
-    if job_title:
-        data = data[data['Job Title'] == job_title]
 
     # Calculate job listings and unique industries
     total_job_listings = len(data)
@@ -118,13 +95,17 @@ def calculate_summary_stats(data, job_title=None):
     return summary_stats
 
 
-# Function to aggregate counts as well as compute summary statistics
-def aggregate_data(data, job_title=None, education_level=None):
-    skill_counts = aggregate_query(Skill, Skill.skill, job_title)
-    tool_counts = aggregate_tools(job_title)
-    education_level_counts = aggregate_query(EducationLevel, EducationLevel.education_level, job_title)
-    field_of_study_counts = aggregate_field_of_study(job_title)
-    summary_stats = calculate_summary_stats(data, job_title)
+# Function to aggregate counts and compute summary statistics, with the main filtering logic
+def aggregate_data(data, job_title=None):
+    # Job title filtering
+    if job_title:
+        data = data[data['Job Title'] == job_title]
+
+    skill_counts = aggregate_query(Skill, Skill.skill)
+    tool_counts = aggregate_tools()
+    education_level_counts = aggregate_query(EducationLevel, EducationLevel.education_level)
+    field_of_study_counts = aggregate_field_of_study()
+    summary_stats = calculate_summary_stats(data)
 
     return skill_counts, tool_counts, education_level_counts, field_of_study_counts, summary_stats
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,40 +4,18 @@ from .data_analysis import aggregate_data
 # Creates 'main' blueprint for organising routes
 main = Blueprint('main', __name__)
 
-DEFAULT_JOB_TITLE = 'Data Analyst'
-
-# Define view handlers to aggregate records from the respective databases and return in JSON format
-@main.route('/aggregated/skills', methods=['GET'])
-def get_aggregated_skills():
-    job_title = request.args.get('job_title', DEFAULT_JOB_TITLE)
+# Consolidated view handler to aggregate records and also filter the data if a job title is selected by JobFilter.js
+@main.route('/api/data', methods=['GET'])
+def get_filtered_data():
+    job_title = request.args.get('job_title')
     data = current_app.config['DATA']
-    skill_counts, _, _, _, _ = aggregate_data(data, job_title)
-    return jsonify(skill_counts.to_dict(orient='records'))
-
-@main.route('/aggregated/tools', methods=['GET'])
-def get_aggregated_tools():
-    job_title = request.args.get('job_title', DEFAULT_JOB_TITLE)
-    data = current_app.config['DATA']
-    _, tool_counts, _, _, _ = aggregate_data(data, job_title)
-    return jsonify(tool_counts.to_dict(orient='records'))
-
-@main.route('/aggregated/education-levels', methods=['GET'])
-def get_aggregated_education_level():
-    job_title = request.args.get('job_title', DEFAULT_JOB_TITLE)
-    data = current_app.config['DATA']
-    _, _, education_level_counts, _, _ = aggregate_data(data, job_title)
-    return jsonify(education_level_counts.to_dict(orient='records'))
-
-@main.route('/aggregated/fields-of-study', methods=['GET'])
-def get_aggregated_field_of_study():
-    job_title = request.args.get('job_title', DEFAULT_JOB_TITLE)
-    data = current_app.config['DATA']
-    _, _, _, field_of_study_counts, _ = aggregate_data(data, job_title)
-    return jsonify(field_of_study_counts.to_dict(orient='records'))
-
-@main.route('/aggregated/summary', methods=['GET'])
-def get_summary_stats():
-    job_title = request.args.get('job_title', DEFAULT_JOB_TITLE)
-    data = current_app.config['DATA']
-    _, _, _, _, summary_stats = aggregate_data(data, job_title)
-    return jsonify(summary_stats)
+    skill_counts, tool_counts, education_level_counts, field_of_study_counts, summary_stats = aggregate_data(data, job_title)
+    
+    response = {
+        'skills': skill_counts.to_dict(orient='records'),
+        'tools': tool_counts.to_dict(orient='records'),
+        'education_levels': education_level_counts.to_dict(orient='records'),
+        'fields_of_study': field_of_study_counts.to_dict(orient='records'),
+        'summaryStats': summary_stats
+    }
+    return jsonify(response)
